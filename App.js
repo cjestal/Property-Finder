f07fda3d446f5cab1668083711a0ee985b253aa9
Clint James <clint@inspireteam.com>
2018-11-02 15:02:16 +0800

    This is a new commit for what I originally planned to be amended

---
+++ App.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import { createStackNavigator } from 'react-navigation';
+import SearchPage from './SearchPage'
+import SearchResults from './SearchResults'
+
+const App = createStackNavigator({
+    Home: { screen: SearchPage },
+    Results: { screen: SearchResults },
+});
+
+export default App;
